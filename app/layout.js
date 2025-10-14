2177583773f561e42c2d244bc63c15dfce263641
govind-rjunior <govind@globalwonders.co>
2025-10-14 01:00:28 -0700

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "GlobalWonders — Funding & Growth Advisory",
+  description: "Connecting smart funds with game-changing startups.",
+};
+import "./globals.css";
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
